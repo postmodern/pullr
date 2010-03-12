610f9d506b6c3e38ddc191c12d3539d76c0419d4
postmodern <postmodern.mod3@gmail.com>
2010-03-11 19:16:02 -0800

    Expanded out the SCM specs to cover the various supported SCMs.

--- spec/scm_spec.rb
+++ spec/scm_spec.rb
@@ -18,15 +18,75 @@ describe SCM do
     }.should raise_error(UnknownSCM)
   end
 
-  it "should infer the SCM from the scheme of a URI" do
-    uri = Addressable::URI.parse('svn://sourceforge.net')
+  describe "Git" do
+    it "should be known as 'git'" do
+      SCM.lookup(:git).should == SCM::Git
+    end
 
-    SCM.infer_from_uri(uri).should == :sub_version
+    it "should be inferable from the scheme of a URI" do
+      uri = Addressable::URI.parse('git://sourceforge.net')
+
+      SCM.infer_from_uri(uri).should == :git
+    end
+
+    it "should be inferable from the extension of a URI" do
+      uri = Addressable::URI.parse('git@github.com/user/project.git')
+
+      SCM.infer_from_uri(uri).should == :git
+    end
   end
 
-  it "should infer the SCM from the extension of a URI" do
-    uri = Addressable::URI.parse('git@github.com/user/project.git')
+  describe "Mercurial" do
+    it "should be known as 'mercurial'" do
+      SCM.lookup(:mercurial).should == SCM::Mercurial
+    end
 
-    SCM.infer_from_uri(uri).should == :git
+    it "should also be known as 'hg'" do
+      SCM.lookup(:hg).should == SCM::Mercurial
+    end
+
+    it "should be inferable from the scheme of a URI" do
+      uri = Addressable::URI.parse('hg://sourceforge.net')
+
+      SCM.infer_from_uri(uri).should == :mercurial
+    end
+  end
+
+  describe "SubVersion" do
+    it "should be known as 'sub_version'" do
+      SCM.lookup(:sub_version).should == SCM::SubVersion
+    end
+
+    it "should also be known as 'subversion'" do
+      SCM.lookup(:subversion).should == SCM::SubVersion
+    end
+
+    it "should also be known as 'svn'" do
+      SCM.lookup(:svn).should == SCM::SubVersion
+    end
+
+    it "should be inferable from the scheme of a URI" do
+      uri = Addressable::URI.parse('svn://sourceforge.net')
+
+      SCM.infer_from_uri(uri).should == :sub_version
+    end
+
+    it "should be inferable from the 'svn+ssh' URI scheme " do
+      uri = Addressable::URI.parse('svn+ssh://sourceforge.net')
+
+      SCM.infer_from_uri(uri).should == :sub_version
+    end
+  end
+
+  describe "Rsync" do
+    it "should be known as 'rsync'" do
+      SCM.lookup(:rsync).should == SCM::Rsync
+    end
+
+    it "should be inferable from the scheme of a URI" do
+      uri = Addressable::URI.parse('rsync://sourceforge.net')
+
+      SCM.infer_from_uri(uri).should == :rsync
+    end
   end
 end

f62c72cb79e5cf0b3fdcfc37fcb12627c07ef350
Postmodern <postmodern.mod3@gmail.com>
2010-06-15 02:28:28 -0700

    Bundle all dependencies.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,14 @@
 require 'rubygems'
-gem 'rspec', '>=1.3.0'
-require 'spec'
 
+begin
+  Bundler.setup(:runtime, :test)
+rescue Bundler::BundlerError => e
+  STDERR.puts e.message
+  STDERR.puts "Run `bundle install` to install missing gems"
+  exit e.status_code
+end
+
+require 'spec'
 require 'pullr/version'
 
 include Pullr
